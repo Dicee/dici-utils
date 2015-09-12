67c590dbc931b4fc26e7a29037394bb5dde002e8
Dicee <dcourtinot@gmail.com>
2015-09-12 13:25:02 +0100

    Add a showWarning factory method

--- src/com/dici/javafx/DialogsFactory.java
+++ src/com/dici/javafx/DialogsFactory.java
@@ -7,21 +7,28 @@ import org.controlsfx.control.action.Action;
 import org.controlsfx.dialog.Dialogs;
 
 public class DialogsFactory {
-	public static final Action showPreFormattedError(Node owner, String titlePropertyName, String mastheadPropertyName, String msgPropertyName) {
-		return Dialogs.create()
-				.owner(owner)
-				.title(strings.getProperty(titlePropertyName))
-				.masthead(strings.getProperty(mastheadPropertyName))
-				.message(strings.getProperty(msgPropertyName))
-				.showError();
+	public static Action showPreFormattedError(Node owner, String titlePropertyName, String mastheadPropertyName, String msgPropertyName) {
+		return preformattedDialog(owner,titlePropertyName,mastheadPropertyName,msgPropertyName).showError();
 	}
 	
-	public static final Action showError(Node owner, String title, String masthead, String msg) {
+	public static Action showError(Node owner, String title, String masthead, String msg) {
 		return Dialogs.create()
 				.owner(owner)
 				.title(title)
 				.masthead(masthead)
 				.message(msg)
 				.showError();
+	}
+	
+	public static Action showPreFormattedWarning(Node owner, String titlePropertyName, String mastheadPropertyName, String msgPropertyName) {
+		return preformattedDialog(owner,titlePropertyName,mastheadPropertyName,msgPropertyName).showWarning();
+	}
+	
+	private static Dialogs preformattedDialog(Node owner, String titlePropertyName, String mastheadPropertyName, String msgPropertyName) {
+		return Dialogs.create()
+				.owner(owner)
+				.title(strings.getProperty(titlePropertyName))
+				.masthead(strings.getProperty(mastheadPropertyName))
+				.message(strings.getProperty(msgPropertyName));
 	}
 }
