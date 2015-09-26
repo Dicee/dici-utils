4e6a24c0fa7c851f56bee7d71bf4aa09ef34711c
Dicee <dcourtinot@gmail.com>
2015-09-27 00:18:27 +0100

    Better name for factory method + less constraintful signature

--- src/main/java/com/dici/collection/richIterator/PairRichIterator.java
+++ src/main/java/com/dici/collection/richIterator/PairRichIterator.java
@@ -3,21 +3,23 @@ package com.dici.collection.richIterator;
 import static com.dici.check.Check.notNull;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import javafx.util.Pair;
+
 import com.dici.exceptions.ExceptionUtils.ThrowingFunction;
 
 public class PairRichIterator<K,V> extends RichIterator<Pair<K,V>> {
 	private final RichIterator<Pair<K,V>> it;
 
-	public static <X,K,V> PairRichIterator<K,V> create(RichIterator<X> it, ThrowingFunction<X,K> keyFunction, ThrowingFunction<X,V> valueFunction) {
+	public static <X,K,V> PairRichIterator<K,V> pairIterator(Iterator<X> it, ThrowingFunction<X,K> keyFunction, ThrowingFunction<X,V> valueFunction) {
 		notNull(keyFunction);
 		notNull(valueFunction);
-		return new PairRichIterator<>(it.map(x -> new Pair<>(keyFunction.apply(x),valueFunction.apply(x))));
+		return new PairRichIterator<>(RichIterators.wrap(it).map(x -> new Pair<>(keyFunction.apply(x), valueFunction.apply(x))));
 	}
 	
 	PairRichIterator(RichIterator<Pair<K,V>> it) { this.it = notNull(it); }
@@ -29,11 +31,11 @@ public class PairRichIterator<K,V> extends RichIterator<Pair<K,V>> {
 	protected Pair<K,V> nextInternal() throws Exception { return it.next(); }
 	
 	public <V1> PairRichIterator<K,V1> mapValues(ThrowingFunction<V,V1> mapper) {
-		return mapToPair(Pair::getKey,pair -> mapper.apply(pair.getValue())); 
+		return mapToPair(Pair::getKey, pair -> mapper.apply(pair.getValue())); 
 	}
 	
 	public <K1> PairRichIterator<K1,V> mapKeys(ThrowingFunction<K,K1> mapper) {
-		return mapToPair(pair -> mapper.apply(pair.getKey()),Pair::getValue); 
+		return mapToPair(pair -> mapper.apply(pair.getKey()), Pair::getValue); 
 	}
 	
 	public PairRichIterator<K,List<V>> groupByKey() {
@@ -43,8 +45,8 @@ public class PairRichIterator<K,V> extends RichIterator<Pair<K,V>> {
 			map.putIfAbsent(pair.getKey(),new LinkedList<>());
 			map.get(pair.getKey()).add(pair.getValue());
 		}
-		return new PairRichIterator<>(RichIterators.wrap(map.entrySet().stream().map(entry ->  new Pair<>(entry.getKey(),entry.getValue())).iterator()));
+		return new PairRichIterator<>(RichIterators.wrap(map.entrySet().stream().map(entry ->  new Pair<>(entry.getKey(), entry.getValue())).iterator()));
 	}
 	
-	public Map<K,V> toMap() { return stream().collect(Collectors.toMap(Pair::getKey,Pair::getValue)); }
+	public Map<K,V> toMap() { return stream().collect(Collectors.toMap(Pair::getKey, Pair::getValue)); }
 }
