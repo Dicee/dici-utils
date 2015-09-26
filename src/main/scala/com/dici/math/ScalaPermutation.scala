821012ca94d74a2b15732f9d561be87bd9b368dd
Dicee <dcourtinot@gmail.com>
2015-09-26 06:17:49 +0100

    Fixed indentation

--- src/main/scala/com/dici/math/ScalaPermutation.scala
+++ src/main/scala/com/dici/math/ScalaPermutation.scala
@@ -2,7 +2,7 @@ package com.dici.math
 
 class ScalaPermutation(private val perm: Permutation) extends Iterable[Int] {
 	def apply(index: Int) = perm.get(index)
-	
+
 	def iterator = new Iterator[Int] {
 		private val it = perm.iterator
 		def hasNext = it.hasNext
