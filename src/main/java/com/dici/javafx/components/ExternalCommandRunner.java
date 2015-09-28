dfd379a84c607ecb8401adb06bf85f6652eed087
Dicee <dcourtinot@gmail.com>
2015-09-28 14:49:44 +0100

    Task.call should not access a UI component

--- src/main/java/com/dici/javafx/components/ExternalCommandRunner.java
+++ src/main/java/com/dici/javafx/components/ExternalCommandRunner.java
@@ -5,6 +5,7 @@ import static com.dici.javafx.components.Resources.STOP_ACTIVE_ICON;
 import static com.dici.javafx.components.Resources.STOP_INACTIVE_ICON;
 
 import java.io.IOException;
+import java.util.concurrent.Semaphore;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
@@ -21,6 +22,8 @@ public class ExternalCommandRunner extends VBox {
     private final TextArea outputTextArea;
     private Process        runningProcess;
     
+    private final Semaphore runMutex = new Semaphore(1);
+    
     public ExternalCommandRunner() {
         this.stop           = new Button("");
         this.outputTextArea = new TextArea();
@@ -29,33 +32,39 @@ public class ExternalCommandRunner extends VBox {
         super.getChildren().addAll(new ToolBar(stop), outputTextArea);
     }
     
-    public synchronized TextArea getOutputTextArea() { return outputTextArea; }
+    public TextArea getOutputTextArea() { return outputTextArea; }
 
-    public synchronized void run(ProcessBuilder processBuilder) throws IOException, InterruptedException {
+    public void run(ProcessBuilder processBuilder) throws IOException, InterruptedException {
+        runMutex.acquire();
+        setActive();
         ExternalCommandRunner self = this;
         Thread taskThread = new Thread(new Task<Void>() {
             @Override
             public Void call() throws Exception {
-                setActive();
-                try {
-                    runningProcess = processBuilder.start();
-                    
-                    StreamPrinter  inputStream    = new StreamPrinter(runningProcess.getInputStream(), self::handleLog);
-                    StreamPrinter  errorStream    = new StreamPrinter(runningProcess.getErrorStream(), self::handleLog);
-                    outputTextArea.clear();
-                    
-                    new Thread(inputStream).start();
-                    new Thread(errorStream).start();
-                    runningProcess.waitFor();
-                    return null;
-                } finally {
-                    stop.fire();
-                    setInactive();
-                }
+                runningProcess = processBuilder.start();
+                
+                StreamPrinter  inputStream = new StreamPrinter(runningProcess.getInputStream(), self::handleLog);
+                StreamPrinter  errorStream = new StreamPrinter(runningProcess.getErrorStream(), self::handleLog);
+                outputTextArea.clear();
+                
+                new Thread(inputStream).start();
+                new Thread(errorStream).start();
+                runningProcess.waitFor();
+                return null;
+            }
+        
+            @Override protected void cancelled() { super.cancelled(); terminate(); }
+            @Override protected void failed   () { super.failed   (); terminate(); }
+            @Override protected void succeeded() { super.succeeded(); terminate(); }
+            
+            private void terminate() {
+                stop.fire();
+                setInactive();
+                runMutex.release();
             }
         });
+        taskThread.setDaemon(true);
         taskThread.start();
-        taskThread.join();
     }
     
     private void handleLog  (String line) { Platform.runLater(() -> outputTextArea.appendText(line + "\n")); }
