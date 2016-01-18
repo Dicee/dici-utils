f0164f9e2ee4e2d4d85e5294c0e7b73de5a491c6
Dicee <dcourtinot@gmail.com>
2016-01-18 03:28:55 +0000

    Added hashCode and equals to some classes

--- src/main/java/com/dici/math/geometry/geometry2D/Delta.java
+++ src/main/java/com/dici/math/geometry/geometry2D/Delta.java
@@ -11,4 +11,21 @@ public class Delta {
     public Delta plus (Delta delta) { return new Delta(dx + delta.dx, dy + delta.dy); }
     
     @Override public String toString() { return String.format("Delta(%d, %d)", dx, dy);}
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + dx;
+        result = prime * result + dy;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Delta that = (Delta) obj;
+        return dx == that.dx && dy == that.dy;
+    }
 }
--- src/main/java/com/dici/math/geometry/geometry2D/ImmutablePoint.java
+++ src/main/java/com/dici/math/geometry/geometry2D/ImmutablePoint.java
@@ -12,4 +12,21 @@ public class ImmutablePoint {
     public ImmutablePoint move (Delta delta) { return moveX(delta.dx).moveY(delta.dy)     ; }
     
     @Override public String toString() { return String.format("(%d, %d)", x, y); }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + x;
+        result = prime * result + y;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ImmutablePoint that = (ImmutablePoint) obj;
+        return x == that.x && y == that.y;
+    }
 }
