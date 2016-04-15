95e499707869d290125567f665b4224aae93e434
Dicee <dcourtinot@gmail.com>
2016-04-15 01:59:04 +0100

    Small cleanup

--- src/main/java/com/dici/function/Predicates.java
+++ src/main/java/com/dici/function/Predicates.java
@@ -1,14 +1,15 @@
 package com.dici.function;
 
-import static com.dici.check.Check.isPositive;
+import static com.dici.check.Check.isGreaterOrEqual;
+import static com.dici.check.Check.notNegative;
 
+import com.dici.check.Check;
 import com.dici.exceptions.ExceptionUtils.ThrowingPredicate;
 
 public class Predicates {
     public static <X> ThrowingPredicate<X> countdownPredicate(int n) {
-        isPositive(n);
         return new ThrowingPredicate<X>() {
-            private int counter = n;
+            private int counter = notNegative(n);
             @Override public boolean test(X x) { return counter-- > 0; }
         };
     }
