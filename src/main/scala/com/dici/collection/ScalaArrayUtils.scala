6949ce0919df91e4ed89a2a0d60b58916be201aa
Dicee <dcourtinot@gmail.com>
2016-08-20 04:41:43 +0100

    Add Scala utility for pretty printing 2D arrays

--- src/main/scala/com/dici/collection/ScalaArrayUtils.scala
+++ src/main/scala/com/dici/collection/ScalaArrayUtils.scala
@@ -5,5 +5,15 @@ object ScalaArrayUtils {
         val tmp = arr(i)
         arr(i)  = arr(j)
         arr(j)  = tmp
-    }  
+    }
+
+    def printMatrix[T](matrix: Array[Array[T]], paddingChar: Char = ' ', spacingChar: Char = ' ')(formatter: (Int, Int, T) => String) = {
+        val maxLength = (for (i <- 0 until matrix.length ; j <- 0 until matrix(0).length) yield formatter(i, j, matrix(i)(j))).map(_.length).max
+        for (i <- 0 until matrix.length) {
+            for (j <- 0 until matrix(0).length) {
+                print(spacingChar + formatter(i, j, matrix(i)(j)).padTo(maxLength, paddingChar))
+            }
+            println()
+        }
+    }
 }
